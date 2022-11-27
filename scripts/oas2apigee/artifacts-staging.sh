6eec71b06fa1e1cbaafc63ce09362454ca256ad3
ShehabEl-DeenAlalkamy <essamshehab33_3@h-eng.helwan.edu.eg>
2022-11-27 19:43:28 +0200

    feat: add oas file to oas2apigee-artifacts

--- scripts/oas2apigee/artifacts-staging.sh
+++ scripts/oas2apigee/artifacts-staging.sh
@@ -3,9 +3,11 @@
 main() {
     readonly ADO_DEBUG_CMD="##[debug]"
     echo "${ADO_DEBUG_CMD}"creating "${BUILD_ARTIFACTSTAGINGDIRECTORY}"/oas2apigee-artifacts
-    mkdir -p "${BUILD_ARTIFACTSTAGINGDIRECTORY}"/oas2apigee-artifacts
+    mkdir -p "${BUILD_ARTIFACTSTAGINGDIRECTORY}"/oas2apigee-artifacts/oas
     echo "${ADO_DEBUG_CMD}"copying "${PROXYNAME}".zip to "${BUILD_ARTIFACTSTAGINGDIRECTORY}"/oas2apigee-artifacts
     cp ./"${PROXYNAME}".zip "${BUILD_ARTIFACTSTAGINGDIRECTORY}"/oas2apigee-artifacts
+    echo "${ADO_DEBUG_CMD}"copying oas/"${OASFILENAME}" to "${BUILD_ARTIFACTSTAGINGDIRECTORY}"/oas2apigee-artifacts/oas/"${OASFILENAME}"
+    cp oas/"${OASFILENAME}" "${BUILD_ARTIFACTSTAGINGDIRECTORY}"/oas2apigee-artifacts/oas/
     echo "${ADO_DEBUG_CMD}"copying "${PROXYNAME}".json to "${BUILD_ARTIFACTSTAGINGDIRECTORY}"/oas2apigee-artifacts
     cp ./"${PROXYNAME}".json "${BUILD_ARTIFACTSTAGINGDIRECTORY}"/oas2apigee-artifacts
     if [[ -d test/integration || -d tests/integration ]]; then
