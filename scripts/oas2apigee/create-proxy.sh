c0fc0157c7498306b937c3b459d54790990e1f53
ShehabEl-DeenAlalkamy <essamshehab33_3@h-eng.helwan.edu.eg>
2022-11-27 19:44:02 +0200

    feat: add initialTargetEndpoint to generated API proxy

--- scripts/oas2apigee/create-proxy.sh
+++ scripts/oas2apigee/create-proxy.sh
@@ -3,12 +3,25 @@
 main() {
     readonly ADO_DEBUG_CMD="##[debug]"
     echo "${ADO_DEBUG_CMD}"creating "${PROXYNAME}" API proxy..
-    ~/.apigeecli/bin/apigeecli apis create openapi \
-        -o "${ORG}" \
-        -a "${SA_PATH}" \
-        -n "${PROXYNAME}" \
-        -f oas/"${OASFILENAME}" \
-        --add-cors | jq >"${PROXYNAME}".json
+    if [[ -z "${INITIALTARGETENDPOINT}" ]]; then
+        echo "${ADO_DEBUG_CMD}"target endpoint: https://"$(grep -m 1 '\- url' oas/"${OASFILENAME}" | cut -d/ -f3)"
+        ~/.apigeecli/bin/apigeecli apis create openapi \
+            -o "${ORG}" \
+            -a "${SA_PATH}" \
+            -n "${PROXYNAME}" \
+            -f oas/"${OASFILENAME}" \
+            --add-cors | jq >"${PROXYNAME}".json
+    else
+        echo "${ADO_DEBUG_CMD}"target endpoint: "${INITIALTARGETENDPOINT}"
+        ~/.apigeecli/bin/apigeecli apis create openapi \
+            -o "${ORG}" \
+            -a "${SA_PATH}" \
+            -n "${PROXYNAME}" \
+            -f oas/"${OASFILENAME}" \
+            --target-url "${INITIALTARGETENDPOINT}" \
+            --add-cors | jq >"${PROXYNAME}".json
+    fi
+
     jq <"${PROXYNAME}".json
 }
 
